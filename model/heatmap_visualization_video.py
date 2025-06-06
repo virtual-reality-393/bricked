d50371c8344a5199b2941381f1403f21e00b2b83
Rasmusdas <rasmusmkn@gmail.com>
2025-06-06 13:24:00 +0200

    Now supports the new data format

--- model/heatmap_visualization_video.py
+++ model/heatmap_visualization_video.py
@@ -13,10 +13,30 @@ def read_lines_from_file(input_file):
         content = f.read().split("\n")
     return content
 
-def follow_camera(view_ctl, target_center, offset):
-    eye = target_center + offset
-    up = np.array([0, 1, 0])  # Y-up world
-    view_ctl.look_at(target_center, eye, up)
+def extract_data(data):
+    type_info, value = data.split(":")
+
+    type, name = type_info.split("_")
+
+    if type == "POS":
+        res = np.array([float(e) for e in value[1:-1].split(",")])
+
+        if len(res) == 3:
+            res[2] *= -1
+
+        return name, res
+
+    elif type == "S":
+        return name, value
+            
+    elif type == "LIST":
+        return name, [e for e in value.split(",")]
+
+    elif type == "NUM":
+        return name, int(value)
+
+    else:
+        print(f"Unknown type {type} in line: {line}")
 
 camera_offset = np.array([0, 0, 2]) 
 index_to_color = {0:(1,0,0,1),1:(0,1,0,1),2:(0,0,1,1),3:(1,1,0,1),4:(0.4,0.4,1,1),5:(0.4,1,0.4,1),6:(1,1,0.4,1),7:(1,1,1,1),8:(1,0.5,0.5,1),9:(1.0,0.2,0.8,1)}
@@ -34,16 +54,21 @@ HAND_BONE_CONNECTIONS = [[0,1],
                         [25, 39], [39, 40], [40, 41], [41, 42], [42, 47]
                         ]
 
-
+name_to_color = {
+    "red": (1, 0, 0, 1),
+    "green": (0, 1, 0, 1),
+    "blue": (0, 0, 1, 1),
+    "yellow": (1, 1, 0, 1),
+}
 lines = read_lines_from_file("game.log")
 nameToId = {"red":0.0,"green":1.0,"blue":2.0,"yellow":3.0}
 
 colors = ["red","green","blue","yellow"]
-
+event_num = 0
 bricks = {}
 tableSize = (-1,-1)
 points = []
-
+is_event = False
 match = re.match(r"\[.*\]",lines[-2])
 
 elements = match.group().split("|")
@@ -51,6 +76,7 @@ elements = match.group().split("|")
 frameCount = int(elements[2].replace("]",""))
 
 frame_data = [None]*(frameCount+1)
+
 
 for line in lines:
     match = re.match(r"\[.*\]",line)
@@ -74,153 +100,57 @@ for line in lines:
     if frame_data[frame_num] is None:
         frame_data[frame_num] = {}
 
-
-
     input_data = line[match.end():].split(";")
 
+    frame_data[frame_num]["FRAME_NUM"] = frame_num
+    frame_data[frame_num]["TIME"] = time
 
-    for data in input_data:
-        try:
-            type_info,value = data.split(":")
+    if identifier not in frame_data[frame_num]:
+        frame_data[frame_num][identifier] = {}
 
-            type, info = type_info.split("_")
-        except:
-            print(line)
+    if is_event:
+        event_num += 1
+    else:
+        event_num = 0
 
-
-    continue
+    is_event = False
     
-    if "Hand" in identifier:
-        if identifier not in frame_data[frame_num]:
-            frame_data[frame_num][identifier] = {}
-            frame_data[frame_num][identifier]["BONES"] = []
-        
+    
+    for data in input_data:
+        name,val = extract_data(data)
+        if name == "EVENT":
+            is_event = True
+            if type(frame_data[frame_num][identifier]) != type([]):
+                frame_data[frame_num][identifier] = []
 
-        bones = line[match.end():].split(";")
-
-        coords = bones[0].split(":")[1][1:-1].split(",")
-
-        x = float(coords[0])
-        y = 1-float(coords[1])
-
-        if 0 < x < 1 and 0 < y < 1:
-            frame_data[frame_num][identifier]["PALM"] = (x,y)
-
-        for bone in bones[1:]:
-            x,y,z = bone.split(":")[1][1:-1].split(",")
-            frame_data[frame_num][identifier]["BONES"].append((float(x),float(y),-float(z)))
-
-    if identifier == "tablePlane":
-        elements = line[match.end():].split(";")
-        coords = elements[0].split(":")[1].split(",")
-
-        x = float(coords[0])
-        y = 1-float(coords[1])
-        tableSize = (x,y)
-
-        tablePosition = np.array([float(e) for e in elements[1].split(":")[1][1:-1].split(",")])
-
-        tablePosition[2] *= -1
-
-        tableRotation = np.array([float(e) for e in elements[2].split(":")[1][1:-1].split(",")])
-
-
-    if identifier == "head":
-        elements = line[match.end():].split(";")
-        if "FOCAL" in line:
-            fx,fy = elements[0].split(":")[1][1:-1].split(",")
-
-            fx = float(fx)
-            fy = float(fy)
-
-            cx,cy = elements[1].split(":")[1][1:-1].split(",")
-
-            cx = float(cx)
-            cy = float(cy)
+        if is_event:
+            if len(frame_data[frame_num][identifier]) <= event_num:
+                frame_data[frame_num][identifier].append({})
+            frame_data[frame_num][identifier][event_num][name] = val
         else:
-            frame_data[frame_num][identifier] = {}
-            x,y,z = elements[0].split(":")[1][1:-1].split(",")
-            frame_data[frame_num][identifier]["POSITION"] = (float(x),float(y),-float(z))
+            frame_data[frame_num][identifier][name] = val
+                    
 
-            x,y,z,w = elements[1].split(":")[1][1:-1].split(",")
-            frame_data[frame_num][identifier]["ROTATION"] = (float(w),float(x),float(y),float(z))
         
+            
 
+for frame in frame_data:
 
-    if identifier == "StackGeneration":
-        if identifier not in frame_data[frame_num]:
-            frame_data[frame_num][identifier] = {}
-        
-        elements = line[match.end():].split(";")
+    if frame == None:
+        continue
 
-        event_type = elements[0]
+    if "tablePlane" in frame:
+        tableSize = frame["tablePlane"]["PLANE"]
+        tablePosition = frame["tablePlane"]["POSITION"]
+    elif "head" in frame:
+        fx,fy = frame["head"]["FOCAL"]
+        cx,cy = frame["head"]["PRINCIPAL"]
+        break
 
-        if event_type == "GENERATE":
-            if "POINTS" not in frame_data[frame_num][identifier]:
-                frame_data[frame_num][identifier]["POINTS"] = []
-            normCoords = elements[2][1:-1].split(",")
-
-            nx = float(normCoords[0])
-            ny = 1-float(normCoords[1])
-
-            worldCoords = elements[3][1:-1].split(",")
-
-            wx = float(worldCoords[0])
-            wy = float(worldCoords[1])
-            wz = -float(worldCoords[2])
-            frame_data[frame_num][identifier]["POINTS"].append([(nx,ny,0),(wx,wy,wz),elements[4].split(",")])
-
-           
-
-        if event_type == "COMPLETED":
-            if "COMPLETED" not in frame_data[frame_num][identifier]:
-                frame_data[frame_num][identifier]["COMPLETED"] = []
-
-            frame_data[frame_num][identifier]["COMPLETED"].append(int(elements[1]))
-
-        if event_type == "FINISHED":
-            frame_data[frame_num][identifier]["FINISHED"] = True
-
-        # coords = line[match.end():][1:-1].split(",")
-
-        # x = float(coords[0])
-        # y = 1-float(coords[1])
-
-        # frame_data[frameNum][identifier].append([x,y])
-
-    if identifier =="brick":
-
-        values = line[match.end():].split(";")
-
-        coords = values[0][1:-1].split(",")
-
-        x = float(coords[0])
-        y = 1-float(coords[1])
-
-        brickType = values[1]
-
-        if brickType not in ["red","green","blue","yellow"]:
-            continue
-
-        if frame_num not in bricks:
-            bricks[frame_num] = []
-        bricks[frame_num].append(np.array([x,y,nameToId[brickType]]))
-
-    # print(frame_data[frameNum])
-
-if tableSize == (-1,-1):
-    print("Couldn't get table size, using 1,1")
-    tableSize = (1,1)
 
 heatmap_size = (int(250*tableSize[1]), int(250*tableSize[0]))
 scale_factor = 4
 rescaled_size = np.array([heatmap_size[1],heatmap_size[0]])*scale_factor
-
-
-out = cv2.VideoWriter('heatmap.mp4', -1, 3, (rescaled_size[0]*2,rescaled_size[1]))
-
-fig = plt.figure()
-axs = [fig.add_subplot(211),fig.add_subplot(212)]
 
 right_hand_points = []
 right_hand_bone_points = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],]
@@ -285,6 +215,7 @@ view_ctl.convert_from_pinhole_camera_parameters(param, allow_arbitrary=True)
 
 
 stack_geometry = []
+stack_points = []
 
 for frame_idx in tqdm(range(idx,frame_num)):
     
@@ -294,66 +225,17 @@ for frame_idx in tqdm(range(idx,frame_num)):
     org_ctr_params = org_ctr.convert_to_pinhole_camera_parameters()
 
     if "rightHand" in curr_frame_data:
-
+        
         if "PALM" in curr_frame_data["rightHand"]:
             x,y = curr_frame_data["rightHand"]["PALM"]
-
 
             x *= heatmap_size[1]
             y *= heatmap_size[0]
 
             right_hand_points.append((x,y))
-
             rh_heatmap[int(y),int(x)] += 1
-        if "BONES" in curr_frame_data["rightHand"]: 
-            right_hand_bone_points = curr_frame_data["rightHand"]["BONES"]
-        
 
-        # line_set = o3d.geometry.LineSet(
-        #     points=o3d.utility.Vector3dVector(bonePoints),
-        #     lines=o3d.utility.Vector2iVector(HAND_BONE_CONNECTIONS),
-        # )
-        # # Optional: color lines (here, white)
-        # line_colors = np.ones((len(HAND_BONE_CONNECTIONS), 3))
-        # line_set.colors = o3d.utility.Vector3dVector(line_colors)
-
-        # Combine and visualize
-        
-
-        # Change point size
-    
-    if "head" in curr_frame_data:
-        if "POSITION" in curr_frame_data["head"]:
-            rot = R.from_quat(curr_frame_data["head"]["ROTATION"]).as_matrix()
-            pos = np.array(curr_frame_data["head"]["POSITION"])
-
-            pos[1] = pos[1]
-
-            R_correction = o3d.geometry.get_rotation_matrix_from_axis_angle([0, np.pi / 2, 0])
-
-            extrinsic[:3,:3] = (rot@R_correction).T
-            extrinsic[:3,3] = -rot.T@pos
-
-            # head_geometry.translate(-head_geometry.get_center())
-
-            # head_geometry.translate(pos[1])
-
-
-            head_geometry.vertices = o3d.utility.Vector3dVector(np.asarray(org_head_geometry.vertices))
-            head_geometry.triangles = o3d.utility.Vector3iVector(np.asarray(org_head_geometry.triangles))
-            head_geometry.compute_vertex_normals()
-
-
-            head_geometry.transform(extrinsic)
-
-
-            vis.update_geometry(head_geometry)
-
-            # param.extrinsic = extrinsic
-            # param.intrinsic = intrinsics
-            # view_ctl.convert_from_pinhole_camera_parameters(param, allow_arbitrary=True)
-            
-      
+        right_hand_bone_points = [curr_frame_data["rightHand"][bone] for bone in [val for val in curr_frame_data["rightHand"] if "BONE" in val]]
 
     if "leftHand" in curr_frame_data:
         if "PALM" in curr_frame_data["leftHand"]:
@@ -361,22 +243,44 @@ for frame_idx in tqdm(range(idx,frame_num)):
 
             x *= heatmap_size[1]
             y *= heatmap_size[0]
-            left_hand_points.append((x,y))
 
+            left_hand_points.append((x,y))
             lh_heatmap[int(y),int(x)] += 1
 
-        if "BONES" in curr_frame_data["leftHand"]: 
-                left_hand_bone_points = curr_frame_data["leftHand"]["BONES"]
+        left_hand_bone_points = [curr_frame_data["leftHand"][bone] for bone in [val for val in curr_frame_data["leftHand"] if "BONE" in val]]
+            
+    if "head" in curr_frame_data:
+        if "POSITION" in curr_frame_data["head"]:
+            rot = R.from_quat(curr_frame_data["head"]["ROTATION"]).as_matrix()
+            pos = np.array(curr_frame_data["head"]["POSITION"])
 
-    if "StackGeneration" in curr_frame_data:
-        if "POINTS" in curr_frame_data["StackGeneration"]:
-            stack_points = np.array([e[0] for e in curr_frame_data["StackGeneration"]["POINTS"]])
-            stack_points[:,0]*=heatmap_size[1]
-            stack_points[:,1]*=heatmap_size[0]
+            pos[1] *= -1
+            R_correction = o3d.geometry.get_rotation_matrix_from_axis_angle([0, np.pi / 2, 0])
 
+            extrinsic[:3,:3] = rot.T
+            extrinsic[:3,3] = -rot.T@pos
 
-            for i in range(len(curr_frame_data["StackGeneration"]["POINTS"])):
-                _,wp,names = curr_frame_data["StackGeneration"]["POINTS"][i]
+            head_geometry.vertices = o3d.utility.Vector3dVector(np.asarray(org_head_geometry.vertices))
+            head_geometry.triangles = o3d.utility.Vector3iVector(np.asarray(org_head_geometry.triangles))
+            head_geometry.compute_vertex_normals()
+
+            head_geometry.transform(extrinsic)
+            vis.update_geometry(head_geometry)            
+      
+
+    if "stack" in curr_frame_data:
+        for event_data in curr_frame_data["stack"]:
+            if "GENERATE" == event_data["EVENT"]:
+                point = np.r_[event_data["COORDS"],np.array([0])]
+                point[1]*=heatmap_size[0]
+                point[0]*=heatmap_size[1]
+
+                
+
+                stack_points.append(point)
+
+                position = event_data["POSITION"]
+                names = event_data["BRICKS"]
 
                 for j,name in enumerate(names):
 
@@ -385,24 +289,23 @@ for frame_idx in tqdm(range(idx,frame_num)):
                     cube.paint_uniform_color(index_to_color[int(nameToId[name])][:-1])
 
                     cube.compute_vertex_normals()
-                    cube.translate(wp + np.array([0,0.02*(j),0]))
+                    cube.translate(position + np.array([0,0.02*(j),0]))
 
                     vis.add_geometry(cube)
 
                     stack_geometry.append(cube)
+                    
 
+            if "COMPLETED" == event_data["EVENT"]:
+                stack_points[event_data["STACKNUM"]][2] = 1
 
+            if "FINISHED" == event_data["EVENT"]:
+                for geom in stack_geometry:
+                    vis.remove_geometry(geom)
+                
+                stack_geometry = []
+                stack_points = []
 
-
-        if "COMPLETED" in curr_frame_data["StackGeneration"]:
-            for val in curr_frame_data["StackGeneration"]["COMPLETED"]:
-                stack_points[val,2] = 1
-
-        if "FINISHED" in curr_frame_data["StackGeneration"]:
-            for geom in stack_geometry:
-                vis.remove_geometry(geom)
-            
-            stack_geometry = []
 
     rh_np = np.array(right_hand_points)
     lh_np = np.array(left_hand_points)
